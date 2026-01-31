e22a6ca6b8282bb9b346a5322a695e68d1a3476a
santiago1544 <sandovalsanty@outlook.com>
2026-01-31 15:21:33 -0500

    Formulario componente inicio de sesion

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -5,7 +5,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({
   providedIn: 'root',
 })
-export class Authentication {
+export class AuthenticationService {
 
   constructor(public ngFireAuth: AngularFireAuth) { }
 
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { AuthenticationService } from 'src/app/authentication.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  regForm: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder, public loadingCtrl: LoadingController, public authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+    this.regForm = this.formBuilder.group({
+      fullname: ['', [Validators.required]],
+      email: ['', [Validators.required, 
+        Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]],
+      password: ['', [Validators.required,
+        Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[0-8])(?=.*[A-Z]).{8,}")
+      ]],
+    });  
+  }
+
+  get errorControl() {
+    return this.regForm.controls;
+  }
+
+  async signUp() {
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    if (this.regForm?.valid) {
+      const user = await this.authService.registerUser(
+        this.regForm?.value.email,
+        this.regForm?.value.password
+      )
+
+    } else {
+
+    } 
   }
 
 }
