22c0c5e90dad53eddbe937e16b9a0fc5742459bb
santiago1544 <sandovalsanty@outlook.com>
2026-02-01 17:27:12 -0500

    Configuración en ios y android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig : {
+    apiKey: "AIzaSyCKalBNyu1IjohvBklPfZ2qDvltmUf1h30",
+    authDomain: "auth-todolist-5714e.firebaseapp.com",
+    projectId: "auth-todolist-5714e",
+    storageBucket: "auth-todolist-5714e.firebasestorage.app",
+    messagingSenderId: "919271790431",
+    appId: "1:919271790431:web:da154ec678edd23bb988ea",
+    measurementId: "G-0L6V1MEB4T"
+  }
 };
